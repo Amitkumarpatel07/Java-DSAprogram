3e4bae0c9666d1cbd4188d82b9d779a7ce178116
Amit Kumar Patel <amitpatelapn2006@gmail.com>
2025-04-18 00:49:50 +0530

    Create StudentName.java

---
+++ StudentName.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+    String name;
+    int marks;
+}
+
+//public class Main{
+    public static void main(String args[]){
+    Student c = new Student();
+    c.name = "Aman";
+    c.marks = 90;
+    
+    System.out.println("Name: " + c.name);
+    System.out.println("Marks: " + c.marks);
+   }
+//}
